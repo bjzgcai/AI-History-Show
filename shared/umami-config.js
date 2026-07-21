e17698241c93caaa4890292b42895e47819e03e0
dliang <doliang2021@qq.com>
2026-07-21 01:09:45 +0000

    Enable Umami analytics with website ID

--- shared/umami-config.js
+++ shared/umami-config.js
@@ -4,8 +4,8 @@
     if (globalScope.AI_HISTORY_UMAMI_CONFIG) return;
 
     globalScope.AI_HISTORY_UMAMI_CONFIG = {
-        enabled: false,
-        websiteId: '',
+        enabled: true,
+        websiteId: '6080b0e0-ea6d-459f-838d-8b4d446d0746',
         scriptUrl: 'https://museum.bza.edu.cn/umami/script.js',
         hostUrl: 'https://museum.bza.edu.cn/umami',
         autoTrack: true,
